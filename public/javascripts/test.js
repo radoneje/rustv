1324776c3fb1944a0dd1e048159e8648d6e62c24
radoneje <den.shevchenko@gmail.com>
2020-04-04 11:27:55 +0300

    ferfef

---
+++ public/javascripts/test.js
@@ -0,0 +1,79 @@
+window.onload=async function() {
+
+
+    var inputVideo=document.getElementById("srcVideo")
+    inputVideo.muted="mute"
+
+
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.font = "20px Georgia";
+    var vw;
+    var vh;
+
+    inputVideo.addEventListener('loadedmetadata', function() {
+        vw = this.videoWidth || this.width;   // these are on video element itself
+        vh = this.videoHeight || this.height;
+        canvas.width = vw
+        canvas.height = vh;
+    });
+
+    inputVideo.addEventListener("play", draw, false);
+    function draw() {
+        if (inputVideo.paused || inputVideo.ended) {
+            return;
+        }
+        ctx.drawImage(inputVideo, 0, 0, vw, vh);
+
+        ctx.fillText(new Date(), 10, 50);
+        requestAnimationFrame(draw);  // loop anim. using rAF
+    }
+
+    var outStream = canvas.captureStream(30);
+    var inputStream= await navigator.mediaDevices.getUserMedia({audio:true, video:{width:320}});
+    inputVideo.srcObject=inputStream;
+    const ac = new AudioContext();
+    const osc = ac.createOscillator();
+    osc.frequency.value = 150;
+    osc.start();
+
+    var gainNode = ac.createGain();
+    gainNode.gain.value = 0.9;
+    osc.connect(gainNode);
+
+    var inputAudioNode= ac.createMediaStreamSource(inputStream);
+
+    var merger = ac.createChannelMerger(20);
+
+    gainNode.connect(merger, 0, 1);
+    inputAudioNode.connect(merger, 0, 0);
+
+
+    const audioStreamDestination = ac.createMediaStreamDestination();
+    merger.connect(audioStreamDestination);
+
+    const newAudioTrack = audioStreamDestination.stream.getAudioTracks()[0];
+
+    var middleVideoStream=canvas.captureStream(30)
+    const newStream = new window.MediaStream();
+
+    newStream.addTrack(newAudioTrack);
+    newStream.addTrack(middleVideoStream.getVideoTracks()[0]);
+
+    var outVideo=document.getElementById("destVideo")
+    outVideo.srcObject=newStream;
+
+var i=100;
+    document.getElementById('btn').addEventListener('click',()=>{
+        i+=100;
+      /*  var osc = ac.createOscillator();
+        osc.frequency.value =i;
+        osc.start();
+        osc.connect(audioStreamDestination);*/
+      //  newStream.addTrack( inputStream.getAudioTracks()[0]);
+
+
+    })
+
+
+}
