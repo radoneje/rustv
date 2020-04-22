098e40a305f097f7f793fada5ece995b977fda22
radoneje <den.shevchenko@gmail.com>
2020-04-22 23:49:54 +0300

    ferfefdsvevev

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(fileUpload({
 }));
 
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
