d77ff32d6ae963fcab0d37d446e8b766a15f7571
radoneje <den.shevchenko@gmail.com>
2020-05-17 19:47:11 +0300

    ferfefdsvevev

--- public/javascripts/phoner.js
+++ public/javascripts/phoner.js
@@ -39,7 +39,7 @@ function publishStream(streamName, localVideo, stream,errHandeler) {
     if(stream) {
         var tracks=stream.getTracks();
         audio = tracks.filter(t => t.kind == "audio").length > 0 ? true : false;
-        video=tracks.filter(t => t.kind == "video").length > 0 ? {width:720} : false;
+        video=tracks.filter(t => t.kind == "audio").length > 0 ? {width:720} : false;
        // console.log("tracks",tracks )
       // audio:false;
       // video:true;
