ea1f6f6c00d9f44087a5b837f87f8a48b60605e8
radoneje <den.shevchenko@gmail.com>
2020-05-19 16:34:05 +0300

    ferfefdsvevev

--- public/javascripts/roomScript.js
+++ public/javascripts/roomScript.js
@@ -366,7 +366,7 @@ window.onload=function () {
                 else if(document.getElementById('profIframe')){
                     var elem=document.getElementById('profIframe')
                     if(val)
-                        elem.src="https://v4.proofix.ru/cbr_rus/embed.html?realtime=true&autoplay=true&mute=false"
+                        elem.src="https://v4.proofix.ru/cbr_rus/embed.html?realtime=true&autoplay=true"
                     else
                         elem.src="https://v4.proofix.ru/cbr_rus/embed.html?realtime=true&mute=true&autoplay=true"
                     console.log("mute",val);
