22a29c0fea2d3bb5eb7bca738b18f56f812912a2
radoneje <den.shevchenko@gmail.com>
2020-05-20 14:00:40 +0300

    ferfefdsvevev

--- public/javascripts/phoner.js
+++ public/javascripts/phoner.js
@@ -125,7 +125,7 @@ async function publishStream(streamName, localVideo, stream,errHandeler, failedH
             constraints: constraints,
             cvoExtension: true,
            // stripCodecs: "h264,H264,opus,vorbis,Opus,Vorbis"
-          //  stripCodecs: "h264,H264"
+            stripCodecs: "h264,H264"
         }).on(STREAM_STATUS.PUBLISHING, function (stream) {
            // alert(1)
             var video=localVideo.querySelector('video')
@@ -182,7 +182,7 @@ async function  phoneGetRemoteVideo(remoteVideo,id, errHandeler) {
         name: id,
         display: remoteVideo,
      //   stripCodecs: "h264,H264,opus,vorbis,Opus,Vorbis"
-    //    stripCodecs: "h264,H264"
+        stripCodecs: "h264,H264"
     })
     .on(STREAM_STATUS.PENDING, function (stream) {
           //  var video = document.getElementById(stream.id());
