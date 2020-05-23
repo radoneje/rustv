fc2dcd66555b5a367261f1c56b11bc155ef10d0c
radoneje <den.shevchenko@gmail.com>
2020-05-23 12:26:28 +0300

    ferfefdsvevev

--- routes/phonerhooksRouter.js
+++ routes/phonerhooksRouter.js
@@ -5,10 +5,17 @@ var axios = require('axios')
 router.post('/:method', async (req, res, next) => {
 
     console.log("phoder rest call", req.params.method)
-    res.json(req.body);
+    switch (req.params.method) {
+        case 'StreamStatusEvent':await OnStreamStatusEvent(req, res);
+        default: res.json(req.body);
+    }
 })
 router.get('/:method', async (req, res, next) => {
 
     res.json('ok');
 });
+
+async function OnStreamStatusEvent(req, res){
+    console.log("phoder rest OnStreamStatusEvent", req.body)
+}
 module.exports = router;
