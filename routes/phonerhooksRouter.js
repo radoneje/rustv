16b8693b88f11f8f6709deb4e21a92893dbb42ea
radoneje <den.shevchenko@gmail.com>
2020-05-23 12:18:32 +0300

    ferfefdsvevev

--- routes/phonerhooksRouter.js
+++ routes/phonerhooksRouter.js
@@ -4,6 +4,7 @@ var axios = require('axios')
 
 router.post('/:method', async (req, res, next) => {
 
+    console.log("phoder rest call", req.params.method)
     res.json(req.body);
 })
 router.get('/:method', async (req, res, next) => {
