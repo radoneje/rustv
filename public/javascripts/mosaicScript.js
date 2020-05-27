cad16c620584527cfc7c5dfa83fa7c8eb1dafe88
radoneje <den.shevchenko@gmail.com>
2020-05-27 12:05:33 +0300

    ferfefdsvevev

--- public/javascripts/mosaicScript.js
+++ public/javascripts/mosaicScript.js
@@ -40,11 +40,11 @@ window.onload=function () {
         mounted:async function () {
             var _this=this;
             document.getElementById("app").style.opacity = 1;
-            axios.get("/rest/api/users/"+eventid+"/"+roomid)
+           /* axios.get("/rest/api/users/"+eventid+"/"+roomid)
                 .then(function (r) {
                     console.log("users", r.data)
                     _this.users = r.data;
-                });
+                });*/
             var serverUrl;
             var scheme = "http";
             if (document.location.protocol === "https:") {
