913ddfebdb7d6cdfa9816e1f71617029784c0b9e
radoneje <den.shevchenko@gmail.com>
2020-06-01 11:30:22 +0300

    ferfefdsvevev

--- public/javascripts/roomScript.js
+++ public/javascripts/roomScript.js
@@ -914,7 +914,7 @@ window.onload=function () {
             window.addEventListener('scroll', function() {
                 console.log("scroll",_this.$refs.youtube)
                 if(_this.$refs.youtube)
-                    checkVisible(document.getElementById())
+                    console.log("checkVisible", checkVisible(_this.$refs.youtube));
             });
 
         }
