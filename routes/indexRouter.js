9e96e2e48ef15e341a2a52d1d023dc2ff18f5664
radoneje <den.shevchenko@gmail.com>
2020-06-07 14:02:41 +0300

    ferfefdsvevev

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -86,7 +86,7 @@ router.get('/room/:id',  async (req, res, next) =>{
     return res.redirect("/login/"+room.eventid+"?redirect="+encodeURI('/room/'+req.params.id))
   var events=await req.knex.select("*").from("t_events").where({id:room.eventid})
 res.header("X-Frame-Options","")
-  res.render('room', { title: 'ON.event '+room.title, room:room , event:events[0]});
+  res.render('room', { title: 'ON.event '+room.title, room:room , event:events[0], lang:(require("../lang.json"))[events[0].lang||"ru"]});
 
 })
 router.get('/stage/:id',  async (req, res, next) =>{
