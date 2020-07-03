fff3d91aabf2b97de5cdd69af28df30f7a384f77
radoneje <den.shevchenko@gmail.com>
2020-07-03 16:43:46 +0300

    ferfefdsvevev

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -515,6 +515,7 @@ router.post("/roomUsers/:roomid",async (req, res, next)=>{
 
 router.get("/users/:eventid/:roomid", checkLoginToRoom, async (req, res, next) => {
 
+
     var ret = [];
     req.transport.clients.forEach(c => {
         if (c.roomid == req.params.roomid && c.isActive)
@@ -523,7 +524,7 @@ router.get("/users/:eventid/:roomid", checkLoginToRoom, async (req, res, next) =
                 i: c.user.i,
                 f: c.user.f,
                 smi:c.user.smi,
-              //  isActive: c.isActive ? true : false,
+                isActive: c.isActive ? true : false,
                 isVideo: c.isVideo,
                 handUp: c.handUp ? true : false,
                 socketid:c.socket.id
