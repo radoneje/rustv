958548f5fbad9a4b3a42e51b47878a4b865c6c47
radoneje <den.shevchenko@gmail.com>
2020-07-24 14:03:56 +0300

    ferfefdsvevev

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -588,7 +588,7 @@ router.get("/spkToVmix/:roomid", async (req, res, next) => {
     var ret=[]
     status.SPKvksUsers.forEach(u=>{
         var user=u.user;
-        ret.push({id:user.id,fi:(user.f||'')+' '+(user.i||''),smi:user.smi, fi_eng:(user.f_eng||'')+' '+(user.i_eng||''),smi_eng:user.smi_eng });
+        ret.push({id:user.id,fi:(user.f||'')+' '+(user.i || ''),smi:user.smi, fi_eng:(user.f_eng  | '')+' '+(user.i_eng | ''),smi_eng:(user.smi_eng | '') });
     })
     if(ret.length==0)
         ret.push({id:'',fi:'',smi:'', fi_eng:'',smi_eng:''});
