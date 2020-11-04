d73509045516bb8d5d4ecc1b685213f882544f75
radoneje <den.shevchenko@gmail.com>
2020-11-04 18:08:26 +0300

    ferfefdsvevev

--- public/javascripts/stageScript.js
+++ public/javascripts/stageScript.js
@@ -1986,55 +1986,58 @@ window.onload=function () {
         }
     }
     var momentFormat = 'HH:mm:ss';
-    if(!isPgm)
-        var momentMask = IMask(document.getElementById('stageTimerLimit'), {
-            mask: Date,
-            pattern: momentFormat,
-            lazy: false,
-            min: new Date(1970, 0, 1),
-            max: new Date(2030, 0, 1),
+    if(!isPgm )
+       setTimeout(()=>{
+           var momentMask = IMask(document.getElementById('stageTimerLimit'), {
+               mask: Date,
+               pattern: momentFormat,
+               lazy: false,
+               min: new Date(1970, 0, 1),
+               max: new Date(2030, 0, 1),
 
-            format: function (date) {
-                return moment(date).format(momentFormat);
-            },
-            parse: function (str) {
-                return moment(str, momentFormat);
-            },
+               format: function (date) {
+                   return moment(date).format(momentFormat);
+               },
+               parse: function (str) {
+                   return moment(str, momentFormat);
+               },
 
-            blocks: {
-                YYYY: {
-                    mask: IMask.MaskedRange,
-                    from: 1970,
-                    to: 2030
-                },
-                MM: {
-                    mask: IMask.MaskedRange,
-                    from: 1,
-                    to: 12
-                },
-                DD: {
-                    mask: IMask.MaskedRange,
-                    from: 1,
-                    to: 31
-                },
-                HH: {
-                    mask: IMask.MaskedRange,
-                    from: 0,
-                    to: 23
-                },
-                mm: {
-                    mask: IMask.MaskedRange,
-                    from: 0,
-                    to: 59
-                }
-                ,
-                ss: {
-                    mask: IMask.MaskedRange,
-                    from: 0,
-                    to: 59
-                }
-            }
-        });
+               blocks: {
+                   YYYY: {
+                       mask: IMask.MaskedRange,
+                       from: 1970,
+                       to: 2030
+                   },
+                   MM: {
+                       mask: IMask.MaskedRange,
+                       from: 1,
+                       to: 12
+                   },
+                   DD: {
+                       mask: IMask.MaskedRange,
+                       from: 1,
+                       to: 31
+                   },
+                   HH: {
+                       mask: IMask.MaskedRange,
+                       from: 0,
+                       to: 23
+                   },
+                   mm: {
+                       mask: IMask.MaskedRange,
+                       from: 0,
+                       to: 59
+                   }
+                   ,
+                   ss: {
+                       mask: IMask.MaskedRange,
+                       from: 0,
+                       to: 59
+                   }
+               }
+           });
+       },200)
+
 }
 
 
