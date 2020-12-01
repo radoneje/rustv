be97c58790553644303ad8009a27c3f0aac6740c
radoneje <den.shevchenko@gmail.com>
2020-12-01 20:18:33 +0300

    ferfefdsvevev

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2101,7 +2101,7 @@ async function sendEmail(email, text) {
     }
 }
 router.post('/regToGpn/',  async (req, res, next) =>{
-    if(Number.isInteger(req.body.code))
+    if(!Number.isInteger(req.body.code))
         return res.json(0);
     console.log(req.body)
     var codes=await req.knex.select("*").from("t_gpncodes").where({id:req.body.code});
