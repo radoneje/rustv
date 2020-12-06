25d4128448942387b411da4965edf1b5267940e6
radoneje <den.shevchenko@gmail.com>
2020-12-06 20:05:42 +0300

    ferfefdsvevev

--- public/javascripts/cbrPlayerScript.js
+++ public/javascripts/cbrPlayerScript.js
@@ -2,13 +2,27 @@ window.onload=function () {
     var player = videojs('my-video');
     player.src('https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4')
 
+    var eventid=27;
+    var roomid=100;
 
     var app= new Vue({
         el:"app",
-        data: {},
-        methods:{},
+        data: {
+            votes:[]
+        },
+        methods:{
+            UpdateInteractive:async function(){
+                try {
+                }
+                catch (e) {
+                    var r = await axios.get("/rest/api/votes/" + eventid + "/" + roomid)
+                    this.votes = r.data;
+                }
+            }
+        },
         mounted:function () {
             document.getElementById("videoWrapper").style.display="block";
+            this.UpdateInteractive();
         }
     })
 }
