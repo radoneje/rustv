e6d2dccdb44f357c21e389685ec9ca2584cb5a35
radoneje <den.shevchenko@gmail.com>
2020-12-06 20:55:19 +0300

    ferfefdsvevev

--- public/javascripts/cbrPlayerScript.js
+++ public/javascripts/cbrPlayerScript.js
@@ -13,6 +13,40 @@ window.onload=function () {
             currLang:lang[langid],
         },
         methods:{
+            vote:function (answ, voteItem) {
+                if(voteItem.iscompl)
+                    return;
+
+                var _this=this;
+                var isReady=localStorage.getItem("ansv_"+answ.id)
+                if(isReady){
+                    localStorage.removeItem("ansv_"+answ.id)
+                    unvote(answ.id)
+                }
+                else
+                {
+                    _this.votes.forEach(v=>{
+                        if(v.id==answ.voteid){
+                            v.answers.forEach(a=>{
+                                if(localStorage.getItem("ansv_"+a.id)) {
+                                    localStorage.removeItem("ansv_" + a.id)
+                                    unvote(a.id)
+                                }
+                            })
+                        }
+                    })
+                    localStorage.setItem("ansv_"+answ.id, true);
+                    vote(answ.id)
+                }
+                _this.votes=_this.votes.filter(v=>{return true})
+                function vote(id) {
+                    axios.post("/rest/api/vote/"+eventid+"/"+roomid,{id:id})
+                }
+                function unvote(id) {
+                    axios.post("/rest/api/unvote/"+eventid+"/"+roomid,{id:id})
+                }
+
+            },
             answIsReady:function (answ) {
                 return localStorage.getItem("ansv_"+answ.id)? true: false
             },
