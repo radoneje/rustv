210fcdb7ad2b5c4d56743b66d022e9132d58c6c3
radoneje <den.shevchenko@gmail.com>
2021-02-08 12:30:09 +0300

    ferfefdsvevev

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(session({
   cookie: {
     maxAge: 10 * 24 * 60 * 60 * 1000,
     sameSite:'none',
+    secure: false,
     //secure:true,
   }, // 10 days
   store:new pgSession(pgStoreConfig),
