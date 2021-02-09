692d7a3da047dfcb349738e713397fcf86db5af1
radoneje <den.shevchenko@gmail.com>
2021-02-09 16:35:22 +0300

    ferfefdsvevev

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2187,5 +2187,23 @@ router.post("/sbergileRegUser",async (req, res, next) => {
         return res.json(usr[0]);
     }
 });
+router.post("/fasRegUser",async (req, res, next) => {
+
+    var rooms=await req.knex.select("*").from("t_rooms").where({isDeleted:false, id:req.body.roomid})
+    var room=rooms[0]
+    if((room.id>=106 && room.id<=107))
+    {
+        var usr = await req.knex("t_eventusers").insert({
+            eventid: req.body.roomid,
+            f: req.body.username,
+            i: "",
+            tel: "",
+            email: "",
+            smsCode: "",
+        }, "*")
+
+        return res.json(usr[0]);
+    }
+});
 
 module.exports = router
